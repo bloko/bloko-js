3ce41355c44703496b8a9a562c2b6b8798f6d9be
Matheus Marques <marquesm91@gmail.com>
2020-05-27 20:29:15 -0300

    chore: export merge from core

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import createStore from './createStore';
 import createUnit from './createUnit';
 import * as globalState from './utils/globalState';
+import merge from './utils/merge';
 
 export default {
   create: createUnit,
   createStore,
 };
 
-export { globalState };
+export { globalState, merge };
